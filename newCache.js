661d49a456954be1960fda68445f6e81db916eca
Bardi <79906840+Fisny@users.noreply.github.com>
2025-04-19 15:29:52 +0200

    ugo

---
+++ newCache.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    const timestamp = Date.now();
+    const selector = 'link[rel="stylesheet"], script[src]';
+
+    document.querySelectorAll(selector).forEach(el => {
+        const attr = el.tagName === 'SCRIPT' ? 'src' : 'href';
+        const val = el.getAttribute(attr);
+        if (val && val.includes('<DATE_HERE>')) {
+            el.setAttribute(attr, val.replace('<DATE_HERE>', timestamp));
+        }
+    });
+})();
