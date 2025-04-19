c85f0232b5e7bc3241ab0ac4eece6630baab5eca
Bardi <79906840+Fisny@users.noreply.github.com>
2025-04-19 15:09:33 +0200

    c# to static html sorry all no money to host

---
+++ about.js
@@ -0,0 +1,15 @@
+﻿function calculateAge(birthDate) {
+    const today = new Date();
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDifference = today.getMonth() - birthDate.getMonth();
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const birthDate = new Date(1999, 4, 8);
+    const age = calculateAge(birthDate);
+    document.getElementById("aboutAge").textContent = age;
+}) 
